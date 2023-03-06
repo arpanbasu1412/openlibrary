b26dd30a3adb621ad477929db567802fd2dfb39c
Arpan <mailmepurabi78@gmail.com>
2023-03-06 21:38:40 +0530

    Add all components in ui Folder

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,11 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookComponent } from './book/book.component';
+import { AdminComponent } from './admin/admin.component';
+import { AuthorComponent } from './author/author.component';
+import { LoanComponent } from './loan/loan.component';
+import { StudentComponent } from './student/student.component';
+import { PublisherComponent } from './publisher/publisher.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookComponent
+    BookComponent,
+    AdminComponent,
+    AuthorComponent,
+    LoanComponent,
+    StudentComponent,
+    PublisherComponent
   ],
   imports: [
     BrowserModule,
